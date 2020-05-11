420f6b2aa350e1642be9ea21551e5dc6680cea3c
StevenVallarsa <orionsjv@gmail.com>
2020-05-10 23:52:52 -0400

    Practice 2020-05-10

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numList = new List<double>() { 1.1, 2, 2, 3.3 };
+
+            double result = 0;
+
+            foreach (var num in numList)
+            {
+                result += num;
+            }
+
+            Console.WriteLine($"The result is {result / numList.Count:N1}");
         }
+
+    }
+
+    public class Book
+    {
+
     }
 }
